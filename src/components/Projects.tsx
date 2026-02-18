88efba7dd31bf23d285e7ae5ef837dd1bf1e416e
Nicholas Eikenberg <nicholaseikenberg@Nicholass-MacBook-Air.local>
2026-02-18 16:17:16 -0500

    fills out the projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,25 @@
+import { projects } from '../util/projects'
+
 export default function Projects() {
   return (
     <>
-      <div className="bg-gray-950 flex justify-center text-center p-4">
-        <h2 className="text-xl bg-gray-300 text-black px-4">Personal Projects</h2>
+      <div className="bg-gray-950 flex justify-center text-center p-4 w-full">
+        <h2 className="text-xl bg-[#d2d2d2] text-black px-4 py-2 w-1/2">PROJECTS</h2>
       </div>
-      <p className="p-4">
-        Project info goes here
-      </p>
+      <div className="font-thin">
+        {projects.map((project, i) => (
+          <section className="p-4 bg-black m-4 text-xs space-y-2" key={i}>
+            <h3 className="text-base">{project.title}</h3>
+            <div className="w-full flex justify-center">
+              <div className="w-1/2 border-4 border-[#1a2732]">
+                <img src={project.image} />
+              </div>
+            </div>
+            <p>{project.description}</p>
+            <p className='text-right'><a href={project.link}>Link</a> | <a href={project.github}>GitHub</a></p>
+          </section>
+        ))}
+      </div>
     </>
   )
 }
---
+++ src/util/projects.tsx
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    title: "Who's That Pokémon?",
+    description: "Who's That Pokémon? is a guessing game utilizing the PokeAPI. The user is shown a random Pokemon's silhouette and must correctly type the name of that Pokemon to earn points. Built with Vanilla JS, JQuery, HTML, and CSS.",
+    image: "src/assets/Pokemon.png",
+    link: "https://nervous-raman-502e99.netlify.app/",
+    github: "https://github.com/NickEikenberg/Project-1"
+  }
+]
