add0e91ac6f3d9ab115b98b3ca53bda103310c94
NickEikenberg <nickeikenberg95@gmail.com>
2026-04-04 17:29:18 -0400

    dumb commit

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -9,6 +9,8 @@ export default function Resume() {
       <div className="font-thin overflow-scroll">
         <section className="p-4 bg-black m-4 text-xs">
           <h3 className="text-base">Technologies</h3>
+          <h2>Some text</h2>
+          <h3>some bullcrap</h3>
           <p>JavaScript, TypeScript, React, Redux, React Native, Next.js, Ruby, Rails, Python, Tailwind CSS,
           Jest, RSpec, AWS, Git, CI/CD (GitHub Actions, Buddy), Sentry,, Azure, PostgreSQL, MongoDB</p>
         </section>
