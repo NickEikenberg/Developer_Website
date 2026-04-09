67934627bc62286676ba9122f14ea53b7d0921ba
NickEikenberg <nickeikenberg95@gmail.com>
2026-04-08 23:04:21 -0400

    removes bad test code

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -9,9 +9,6 @@ export default function Resume() {
       <div className="font-thin overflow-scroll">
         <section className="p-4 bg-black m-4 text-xs">
           <h3 className="text-base">Technologies</h3>
-          <h2>Some text</h2>
-          <h3>some bullcrap</h3>
-          <h1>Something New</h1>
           <p>JavaScript, TypeScript, React, Redux, React Native, Next.js, Ruby, Rails, Python, Tailwind CSS,
           Jest, RSpec, AWS, Git, CI/CD (GitHub Actions, Buddy), Sentry,, Azure, PostgreSQL, MongoDB</p>
         </section>
